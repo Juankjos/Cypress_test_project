ab1db2a41b27196ea8eee3440ebc0184b845c5ef
Juankjos <juanca_jos@live.com.mx>
2023-02-24 17:39:38 -0600

    First real test

--- cypress/e2e/Logintest/archivisticaLogin.cy.js
+++ cypress/e2e/Logintest/archivisticaLogin.cy.js
@@ -1,11 +1,11 @@
 describe('Archivistica Login Test', ()=>{
-    let name = "Juanet";
+    let name = "Archivo de clasificacion de Prueba";
 
     beforeEach('Login Archivistica', ()=>{
         cy.LoginArchivistica()
     })
 
-    it('TC1: Testing Archivistica Classification', ()=>{
+    it('TC1: Testing Archivistica Classification Board', ()=>{
         cy.visit("http://192.168.0.227/test_arch/administrator/classifications")
         cy.url().should('contain', "classifications")
         cy.get('[class="font-medium-3 fas fa-plus-circle"]').click()
@@ -13,11 +13,11 @@ describe('Archivistica Login Test', ()=>{
         cy.get('app-classification-modal').find('input').eq(0).type(name)
         cy.get('app-classification-modal').find('button').eq(3).click()
         cy.get('app-classification-modal').find('a').eq(0).click()
-
-        // if('[id="toast-container"]'){
-        //     cy.get('app-classification-modal').find('button').eq(1).click()
-        //     cy.get('[aria-labelledby="swal2-title"]').find('button').eq(0).click()
-        // }
-        // cy.get('.row').find('[strong="Janet"]').eq(0).click()
+        // cy.get('body').then($body =>{
+            if('[id="toast-container"]'){
+                cy.get('app-classification-modal').find('button').eq(1).click()
+                cy.get('[class="swal2-confirm swal2-styled swal2-default-outline"]').click()
+            }
+        // })
     })
 })
