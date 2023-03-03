d61752d95c1b8d9c783f12445928c7ee8a77d731
Juankjos <juanca_jos@live.com.mx>
2023-03-03 14:30:05 -0600

    Docker integration to cypress

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:slim
+RUN apt-get update
+RUN apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+RUN npm install -g cypress --save-dev
+RUN mkdir cypress 
+COPY ./cypress cypress/
+RUN cd cypress/
--- cypress/e2e/Logintest/archivisticaLogin.cy.js
+++ cypress/e2e/Logintest/archivisticaLogin.cy.js
@@ -22,7 +22,7 @@ describe('Archivistica Login Test', ()=>{
     // })
 
     it('TC1: Testing Archivistica Classification Board', ()=>{
-        cy.visit("http://192.168.0.227/test_arch/administrator/classifications/63f949ef23e5f6480c80819b")
+        cy.visit("http://domus.dyndns.biz:5555/test_arch/administrator/classifications/63f949ef23e5f6480c80819b")
         cy.url().should('contain', "classifications")
         cy.get('.ng-value-container').find('input').click()
         // cy.get('[class="ng-dropdown-panel-items scroll-host"]').find('div').find('div').eq(0).click()
